2a338c81806c7d8ff8d0aea8a92cb90bfb2ba130
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 20:12:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeNumber(currentNum, name) {
   if(currentNum < 1){
     return `Welcome, ${name}. You are ${currentNum + 1}st in line.`;
-  }else if(currentNum === 2) {
+  }else if(currentNum < 2) {
     return `Welcome, ${name}. You are ${currentNum + 1}nd in line.`;
   } else if(currentNum === 3){
     return `Welcome, ${name}. You are ${currentNum + 1}rd in line.`;
