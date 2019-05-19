25be0f270f0d0f7eb5653817f48903423c4c75b9
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 20:29:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function takeNumber(num, name) {
   }
 }
 
-var currentLine = 2;
-console.log(takeNumber(currentLine, "Alex"));
-console.log(currentLine);
+// var currentLine = 2;
+// console.log(takeNumber(currentLine, "Alex"));
+// console.log(currentLine);
