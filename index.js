b3bec7972e9dbea325c875dfc85f352bf9b17786
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 20:42:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function takeANumber(deliLine, name) {
 
   // Generate message
   if(deliLine.length < 1){
-    return `Welcome, ${name}. You are ${deliLine.length + 1} in line.`;
+    return `Welcome, ${name}. You are number ${deliLine.length + 1} in line.`;
   }else if(deliLine.length < 2) {
-    return `Welcome, ${name}. You are ${deliLine.length + 1} in line.`;
+    return `Welcome, ${name}. You are number ${deliLine.length + 1} in line.`;
   } else if(deliLine.length < 3){
-    return `Welcome, ${name}. You are ${deliLine.length + 1} in line.`;
+    return `Welcome, ${name}. You are number ${deliLine.length + 1} in line.`;
   }else {
-    return `Welcome, ${name}. You are ${deliLine.length + 1} in line.`;
+    return `Welcome, ${name}. You are number ${deliLine.length + 1} in line.`;
   }
 }
 
