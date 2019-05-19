f351aa2b620ad1e86a0f2385fdaa94b4c2105b20
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 20:55:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function takeANumber(deliLine, name) {
 
 function nowServing(deliLine) {
   if(deliLine.length < 1) {
-    return 
+    return "There is nobody waiting to be served!"
   }
   var myVisitor = deliLine.slice();
   deliLine.shift();
