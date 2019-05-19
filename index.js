40ba9461599619137f36e793d95eb5d03bdc2de6
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 20:28:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function takeNumber(num, name) {
   currentLine += 1;
 
   // Generate message
-  if(currentLine < 1){
+  if(num < 1){
     return `Welcome, ${name}. You are ${num + 1}st in line.`;
-  }else if(currentLine < 2) {
+  }else if(num < 2) {
     return `Welcome, ${name}. You are ${num + 1}nd in line.`;
-  } else if(currentLine < 3){
+  } else if(num < 3){
     return `Welcome, ${name}. You are ${num + 1}rd in line.`;
   }else {
     return `Welcome, ${name}. You are ${num + 1}th in line.`;
