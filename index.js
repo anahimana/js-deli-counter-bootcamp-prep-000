a8c2ebd0c16d9bae4035585fbd09ded1d57163fa
Alex Nahimana <alex.nahimana1@gmail.com>
2019-05-19 20:22:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function takeNumber(num, name) {
   currentLine = num += 1;
 
 
-  // if(currentNum < 1){
-  //   return `Welcome, ${name}. You are ${currentNum + 1}st in line.`;
-  // }else if(currentNum < 2) {
-  //   return `Welcome, ${name}. You are ${currentNum + 1}nd in line.`;
-  // } else if(currentNum < 3){
-  //   return `Welcome, ${name}. You are ${currentNum + 1}rd in line.`;
-  // }else {
-  //   return `Welcome, ${name}. You are ${currentNum + 1}th in line.`;
-  // }
+  if(currentNum < 1){
+    return `Welcome, ${name}. You are ${currentNum + 1}st in line.`;
+  }else if(currentNum < 2) {
+    return `Welcome, ${name}. You are ${currentNum + 1}nd in line.`;
+  } else if(currentNum < 3){
+    return `Welcome, ${name}. You are ${currentNum + 1}rd in line.`;
+  }else {
+    return `Welcome, ${name}. You are ${currentNum + 1}th in line.`;
+  }
 
 }
 
